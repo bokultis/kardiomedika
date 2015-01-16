f5a2ba21dcf06f7507a97d671bf78cba2ad9b407
Milan Rukavina <milan@horisen.com>
2015-01-16 15:54:08 +0100

    grunt added

---
+++ scripts/Gruntfile.js
@@ -0,0 +1,134 @@
+module.exports = function(grunt) {
+    
+    var processFileVersion = function(content, srcpath){
+        var d = new Date();
+        var month = d.getMonth() + 1;
+        var bn = d.getFullYear().toString() + month.toString() + d.getDate().toString() + d.getHours().toString();
+        return content.replace(/\'built_number' => '(\d*)'/g, "'built_number' => '" + bn + "'");            
+    };
+    
+    var config = grunt.file.readJSON('grunt.config.json');
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            css: {
+                src: [  '../public/plugins/bxSlider/bx_styles/jquery.bxslider.css', '../public/css/font-awesome.css',
+                        '../public/plugins/fancybox/jquery.fancybox.css',
+                        '../public/themes/horisen_2015/css/bootstrap.min.css',
+                        '../public/themes/horisen_2015/css/bootstrap-form.css', '../public/themes/horisen_2015/css/bootstrap-grid.css',
+                        '../public/themes/horisen_2015/css/fonts.css', '../public/themes/horisen_2015/css/style.css',
+                        '../public/themes/horisen_2015/css/internal-pages.css', '../public/themes/horisen_2015/css/search.css',
+                        '../public/themes/horisen_2015/css/sticky-footer.css'],
+                dest: '../public/themes/horisen_2015/css/build.css'
+            },
+            js: {
+                options: {
+                    separator: ';'
+                },
+                src: [  '../public/js/head.js', '../public/js/jquery-1.11.1.min.js',
+                '../public/plugins/picturefill/picturefill.js', '../public/plugins/bxSlider/jquery.bxSlider.js',
+                '../public/plugins/fancybox/jquery.fancybox.pack.js',
+                '../public/plugins/collapsibleBox/jquery.collapsibleBox.js', '../public/themes/horisen_2015/js/main.js',
+                '../public/modules/contact/js/jquery.ajaxSubmit.js'],
+                dest: '../public/themes/horisen_2015/js/build.js'                
+            },
+            intro_css: {
+                src: [  '../public/css/font-awesome.css',
+                        '../public/themes/horisen_2015/css/bootstrap-form.css', '../public/themes/horisen_2015/css/bootstrap-grid.css',
+                        '../public/themes/horisen_2015/css/fonts.css', '../public/themes/horisen_2015/css/style.css',
+                        '../public/themes/horisen_2015/css/intro.css'],
+                dest: '../public/themes/horisen_2015/css/intro_build.css'
+            },
+            intro_js: {
+                options: {
+                    separator: ';'
+                },                
+                src: [  '../public/js/head.js', '../public/js/jquery-1.11.1.min.js',
+                '../public/plugins/picturefill/picturefill.js', '../public/plugins/mousewheel/jquery.mousewheel.min.js',
+                '../public/plugins/sequence/jquery.sequence-min.js',
+                '../public/themes/horisen_2015/js/intro.js'],
+                dest: '../public/themes/horisen_2015/js/intro_build.js'                
+            }            
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },            
+            dist: {
+                files: {
+                    '../public/themes/horisen_2015/js/build.min.js': ['../public/themes/horisen_2015/js/build.js'],
+                    '../public/themes/horisen_2015/js/intro_build.min.js': ['../public/themes/horisen_2015/js/intro_build.js']
+                }
+            }
+        },
+        cssmin : {
+            css:{
+                src: '../public/themes/horisen_2015/css/build.css',
+                dest: '../public/themes/horisen_2015/css/build.min.css'
+            },
+            intro_css:{
+                src: '../public/themes/horisen_2015/css/intro_build.css',
+                dest: '../public/themes/horisen_2015/css/intro_build.min.css'
+            }            
+        },
+        copy: {
+            version: {
+                src: '../application/version.php',
+                dest: '../application/version.php',
+                options: {
+                    process: processFileVersion
+                }              
+            }          
+        },
+        exec:{
+            svn_update: {
+                cmd: 'svn up ../'
+            },            
+            deploy: {
+                cmd: function(mode, host) {                  
+                    var modeParam = (mode == 'upload')? '': ' -n';
+                    
+                    if(!config.rsync.hosts[host]){
+                        grunt.fail.fatal('[' + host + "] host not found in config file");
+                    }                    
+                    var hostConfig = config.rsync.hosts[host];
+                    var command = hostConfig.command.replace('rsync', 'rsync' + modeParam);
+                    grunt.log.writeln("Running deploy in mode:" + mode + " on host:" + host);
+                    grunt.log.writeln("Command: " + command);
+                    return command;
+                }
+            },
+            svn_commit: {
+                cmd: 'svn commit -m "grunt new build" ../'
+            }            
+        },
+        imagemin: {
+            options: {
+                cache: false
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'dist/'
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('default', ['exec:svn_update', 'concat', 'uglify', 'cssmin', 'copy', 'exec:svn_commit']);
+    
+    var mode = grunt.option('mode') || 'simulation';
+    var host = grunt.option('host') || 'staging';
+    grunt.registerTask('deploy', ['exec:deploy:' + mode + ':' + host]);
+
+};
