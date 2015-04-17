d8bd99f4d068affdcfa8491ce5dcfd27bd2e8388
Boris Kundacina <boris@horisen.com>
2015-04-17 10:08:15 +0200

    gcs and index controller issues

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -4,8 +4,6 @@ class IndexController extends HCMS_Controller_Action_Cms
 {
     public function indexAction()
     {
-        if($this->getRequest()->getParam('module') != 'default'){
-            throw new Zend_Controller_Action_Exception($this->translate("Page not found"),404);
-        }
+        throw new Zend_Controller_Action_Exception($this->translate("Page not found"),404);
     }           
 }
