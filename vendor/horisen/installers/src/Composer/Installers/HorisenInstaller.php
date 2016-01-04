33b120de6939c20e146eb3dc48af19c187acca9e
Boris Kundacina <bokultis@gmail.com>
2016-01-04 13:44:33 +0100

    jebemliga

--- vendor/horisen/installers/src/Composer/Installers/HorisenInstaller.php
+++ vendor/horisen/installers/src/Composer/Installers/HorisenInstaller.php
@@ -4,8 +4,10 @@ namespace Composer\Installers;
 class HorisenInstaller extends BaseInstaller
 {
     protected $locations = array(
+    /*
         'cms_lib' => 'library/{$name}/',
         'cms_mod' => 'application/modules/{$name}/',
         'cms_pub_plg' => 'public/{$name}/'
+        */
     );
 }
